d8894bacc0b31ee767f582d197b16e78ccd94857
Sergey Polozov <fiyzor-99@yandex.ru>
2022-07-12 16:13:39 +0500

    спринт 14 итерация 1(3)

--- models/user.js
+++ models/user.js
@@ -1,24 +1,27 @@
-const validator = require('validator');
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
-    role: { type: String, default: 'Жак-Ив Кусто' },
+    type: String,
+    default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
   },
   about: {
-    role: { type: String, default: 'Исследователь' },
+    type: String,
+    default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
+    type: String,
     validate: {
       validator(v) {
         return /^(https?:\/\/(www\.)?([a-zA-z0-9-]{1}[a-zA-z0-9-]*\.?)*\.{1}([a-zA-z0-9]){2,8}(\/?([a-zA-z0-9-])*\/?)*\/?([-._~:?#[]@!\$&'\(\)\*\+,;=])*)/.test(v);
       },
     },
-    role: { type: String, default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png' },
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
